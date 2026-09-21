9bbeb63118786ea14f67fc751689e883b06a4c66
Areeb Iqbal <iqbalareeb26@gmail.com>
2026-09-21 20:08:10 +0500

    chore: sync README and Tauri versions to latest (0.5.68) and fix bump script

--- bump.cjs
+++ bump.cjs
@@ -1,7 +1,7 @@
 const fs = require('fs');
-let p = fs.readFileSync('package.json', 'utf8');
-fs.writeFileSync('package.json', p.replace(/"version": "[^"]+"/, '"version": "0.5.37"'));
+const version = require('./package.json').version;
 let t = fs.readFileSync('src-tauri/tauri.conf.json', 'utf8');
-fs.writeFileSync('src-tauri/tauri.conf.json', t.replace(/"version": "[^"]+"/, '"version": "0.5.37"'));
+fs.writeFileSync('src-tauri/tauri.conf.json', t.replace(/"version": "[^"]+"/, `"version": "${version}"`));
 let c = fs.readFileSync('src-tauri/Cargo.toml', 'utf8');
-fs.writeFileSync('src-tauri/Cargo.toml', c.replace(/version = "[^"]+"/, 'version = "0.5.37"'));
+fs.writeFileSync('src-tauri/Cargo.toml', c.replace(/version = "[^"]+"/, `version = "${version}"`));
+console.log(`Synced Tauri versions to ${version}`);
